850ac9a168627c83ff087f24d618b1fd80ff96c8
Laurent Minguet <laurent@spareka.com>
2025-04-25 11:58:04 +0200

    better test scripts

--- scripts/include/test.sh
+++ scripts/include/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 HOUR=$(date +%H:%M:%S)
 echo "[${HOUR}]===[TEST][CLEAN]==="
 
@@ -17,6 +19,11 @@ do
     PDF_FILE=`echo "$PHP_SCRIPT" | sed "s/\.php/\.pdf/g" | sed "s/\.\//\.\.\/\.\.\/results\/$PHP_VERSION\//g"`
     echo "Example $PHP_SCRIPT => $PDF_FILE"
     php $PHP_SCRIPT > $PDF_FILE
+
+    if ! head -c 8 "$PDF_FILE" | grep -q '^%PDF-1\.7'; then
+        echo " => ERROR - the result file is not a PDF"
+        exit 1
+    fi
 done
 
 HOUR=$(date +%H:%M:%S)
